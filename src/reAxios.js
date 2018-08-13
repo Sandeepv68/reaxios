3f66f0f8d9b81bf050fcb3f75adee016efa10906
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-13 20:07:09 +0530

    core js boiler plate

---
+++ src/reAxios.js
@@ -0,0 +1,18 @@
+/**
+ * reAxios v0.0.1
+ * Axios ajax library wrapped in RxJs for the cool and popular kids
+ * Written by: Sandeep Vattapparambil
+ * Email: sandeepv68@gmail.com
+ * Website: www.sandeepv.in
+ */
+
+ /**
+  * @class ReAxios
+  * The core reAxios class
+  */
+
+  class ReAxios {
+
+  }
+
+  export default ReAxios;
