c527f5937ec68b353e3f0db674b8956df6876a87
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-16 22:18:54 +0530

    updated umd builds

--- dist/reAxios.js
+++ dist/reAxios.js
@@ -134,10 +134,6 @@ return /******/ (function(modules) { // webpackBootstrap
  * Axios library for ajax
  */
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 /**
@@ -350,7 +346,7 @@ var ReAxios = function () {
     return ReAxios;
 }();
 
-exports.default = ReAxios;
+module.exports = ReAxios;
 
 /***/ }),
 
